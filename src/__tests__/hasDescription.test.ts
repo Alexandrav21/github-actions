ed75a7781ba9a66a97840f382a52e3af53ec189c
Alexandra V <alexandra.vladu@thetrainline.com>
2024-02-12 12:46:21 +0000

    removing extra comments

--- src/__tests__/hasDescription.test.ts
+++ src/__tests__/hasDescription.test.ts
@@ -1,7 +1,6 @@
 import Context from '@actions/github/lib/context';
 import { hasDescription } from "../utils/hasDescription";
 
-//  const description = githubContext.payload.pull_request?.body
 describe('hasDescription', () => {
   it('should not throw an error for a valid description', () => {
     expect(() => hasDescription({payload: { pull_request: { body: 'Valid description'}}} as Context )).not.toThrow();
--- src/__tests__/hasTitle.test.ts
+++ src/__tests__/hasTitle.test.ts
@@ -3,76 +3,10 @@ import Context from '@actions/github/lib/context';
 
 describe('hasTitle', () => {
   it('should not throw an error for a valid title', () => {
-    expect(() => hasTitle({ payload: { pull_request: { title: 'haha'} } } as unknown as Context)).not.toThrow();
+    expect(() => hasTitle({ payload: { pull_request: { title: 'Valid title'} } } as unknown as Context)).not.toThrow();
   });
   it('should throw an error for an empty title', () => {
     expect(() => hasTitle({ payload: { pull_request: { title: ''} } } as unknown as Context)).toThrow('The PR must have a title!');
   });
 });
 
-
-
-
-
-
-
-
-// //create a simple interface the mock the context object as title is not working otherwise
-
-
-// const mockContext = {
-//   payload: {
-//     pull_request: {
-//       title: 'Valid title'
-//     }
-//   }
-// } as context;
-// describe('hasTitle', () => {
-//   it('should not throw an error for a valid title', () => {
-//     // Test that hasTitle doesn’t throw an error for a valid title
-//     expect(() => hasTitle(mockContext)).not.toThrow();
-//   });
-//   it('should throw an error for an empty title', () => {
-//     // Adjust the title to test for an empty title
-//     mockContext.payload.pull_request.title = '';
-//     // Test that hasTitle throws an error for an empty title
-//     expect(() => hasTitle(mockContext)).toThrow('The PR must have a title!');
-//   });
-// });
-
-
-// //create a simple interface the mock the context object as title is not working otherwise
-// interface mockContext {
-//   payload: {
-//     pull_request: {
-//       title: string;
-//     }
-//   }
-// } as Context;
-
-// describe('hasTitle', () => {
-//   it('should not throw an error for a valid title', () => {
-//     // Define a mock context object with a valid title
-//     const mockContext = {
-//       payload: {
-//         pull_request: {
-//           title: 'Valid title'
-//         }
-//       }
-//     };
-//     // Test that hasTitle doesn’t throw an error for a valid title
-//     expect(() => hasTitle(mockContext as any)).not.toThrow();
-//   });
-//   it('should throw an error for an empty title', () => {
-//     // Define a mock context object with an empty title
-//     const mockContext = {
-//       payload: {
-//         pull_request: {
-//           title: ''
-//         }
-//       }
-//     };
-//     // Test that hasTitle throws an error for an empty title
-//     expect(() => hasTitle(mockContext as any)).toThrow('The PR must have a title!');
-//   });
-// });
